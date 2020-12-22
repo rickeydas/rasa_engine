280094f7a4875e8de59b48d1cd5fc443a5e43842
Shubham <shubhamchandra067@gmail.com>
2020-12-22 10:38:50 +0530

    code restructured

--- corenlp_parser.py
+++ corenlp_parser.py
@@ -10,29 +10,22 @@ LABELS = labelconfig.label_config["LABELS"]
 def normalize_entity(text):
     url = "http://corenlp.run/?properties=%7B%22annotators%22%3A%20%22tokenize%2Cssplit%2Cpos%2Cner%2Cdepparse%2Copenie%22%2C%20%22date%22%3A%20%222020-12-21T11%3A18%3A47%22%7D&pipelineLanguage=en"
     myobj = {'text': text}
-    
+
     try:
         output = requests.post(url, data = myobj)
         sentences = output.json()["sentences"]
         finalOutput = []
         for sentence in sentences:
             for entity in sentence["entitymentions"]:
-                if 'normalizedNER' in entity:
-                    outputStructure = {
-                        "start": entity["characterOffsetBegin"] - 5,
-                        "end": entity["characterOffsetEnd"] - 5,
-                        "entity": entity["text"],
-                        "type": entity["ner"],
-                        "normalizedNER": "-".join(entity["normalizedNER"].split("T")[0].split("-")[::-1])
-                    }
-                else:
-                    outputStructure = {
+                outputStructure = {
                         "start": entity["characterOffsetBegin"] - 5,
                         "end": entity["characterOffsetEnd"] - 5,
                         "entity": entity["text"],
                         "type": entity["ner"],
                         "normalizedNER": ""
                     }
+                if 'normalizedNER' in entity:
+                    outputStructure["normalizedNER"] = "-".join(entity["normalizedNER"].split("T")[0].split("-")[::-1])
                 finalOutput.append(outputStructure)
         
         for i, item in enumerate(finalOutput):
@@ -50,4 +43,4 @@ def normalize_entity(text):
     except requests.exceptions.RequestException as err:
         return "An Unknown Error occurred" + repr(err)
 
-# print(normalize_entity('my job at California will be done by next monday. I will buy mercedez car next year april. I will go to London by 20 july next year'))
+# print(normalize_entity('my job at California will be done by next monday. I will buy mercedez car next year april. I will go to London by 20 july next year. He might leave today'))
